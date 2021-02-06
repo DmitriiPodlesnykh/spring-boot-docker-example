280456c326299e234d4da03690768272195357b9
Podlesnykh <DSPodlesnykh@luxoft.com>
2021-02-07 01:44:25 +0300

    Initial commit. The simple Spring Boot application was added. There was created endpoint GET "/".

---
+++ src/test/java/com/podlesnykh/example/springbootdockerexample/SpringBootDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podlesnykh.example.springbootdockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
